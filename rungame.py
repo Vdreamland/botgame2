b791ab26023bc98034e7dc5a8aefb0523e0dd29e
Arashi <exterionsx@gmail.com>
2026-07-12 14:02:57 +0700

    Update

--- rungame.py
+++ rungame.py
@@ -6,7 +6,7 @@ sys.path.append(os.path.join(os.path.dirname(__file__), "src"))
 
 from src.connection import connect_and_play
 from src.config import load_active_accounts
-from web.web_server import start_server
+from web.web_server import start_web_server
 
 async def bot_worker(bot_name, api_key, entry_type):
     while True:
@@ -17,7 +17,7 @@ async def bot_worker(bot_name, api_key, entry_type):
         await asyncio.sleep(5)
 
 async def main():
-    asyncio.create_task(start_server())
+    asyncio.create_task(start_web_server())
     await asyncio.sleep(0.5)
     
     accounts = load_active_accounts()
